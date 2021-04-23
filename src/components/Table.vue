7db66cfc58b4e0ffe35db40a293f243f380e6837
Hiroyuki Akimoto <hiroyuki.akimoto@willof.co.jp>
2021-04-23 16:54:15 +0900

    [add]チェックリスト・ソート機能実装土台完成

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,67 +1,79 @@
 <template>
 <div>
-    <div id="box1" class="box">    
+    <div id="box1" class="box">
+        <ul class="check">
+            <li>
+                <input type="checkbox" class="checkbox-main" v-model="checked_all">
+                {{ checked_items }}
+            </li>
+            <li>
+                <input type="checkbox" class="checkbox" v-for="item in items" :key="item" @click="cheked_items">
+            </li>
+        </ul>
         <draggable tag="ul" :options="{group:'ITEMS'}">
-        <li>
-            <div class="title">求人ID</div>
-            <div class="list" v-for="item in items" :key="item.id">
-                <div class="list-nav">{{item.id}}</div>
-            </div>
-        </li>
-        <li>
-            <div class="title">クライアント名</div>
-            <div class="list" v-for="item in items" :key="item.name">
-                <div class="list-nav">{{item.name}}</div>
-            </div>
-        </li>
-        <li>
-            <div class="title">ステータス</div>
-            <div class="list" v-for="item in items" :key="item.status">
-                <div class="list-nav">{{item.status}}</div>
-            </div>
-        </li>
-        <li>
-            <div class="title">人選期日</div>
-            <div class="list" v-for="item in items" :key="item.date">
-                <div class="list-nav">{{item.date}}</div>
-            </div>
-        </li>
-        <li>
-            <div class="title">開始日</div>
-            <div class="list" v-for="item in items" :key="item.satrtDate">
-                <div class="list-nav">{{item.satrtDate}}</div>
-            </div>
-        </li>
-        <li>
-            <div class="title">求人タイトル</div>
-            <div class="list" v-for="item in items" :key="item.title">
-                <div class="list-nav">{{item.title}}</div>
-            </div>
-        </li>
-        <li>
-            <div class="title">最寄駅</div>
-            <div class="list" v-for="item in items" :key="item.station">
-                <div class="list-nav">{{item.station}}</div>
-            </div>
-        </li>
-        <li>
-            <div class="title">雇用期間</div>
-            <div class="list" v-for="item in items" :key="item.period">
-                <div class="list-nav">{{item.period}}</div>
-            </div>
-        </li>
-        <li>
-            <div class="title">研修有無</div>
-            <div class="list" v-for="item in items" :key="item.training">
-                <div class="list-nav">{{item.training}}</div>
-            </div>
-        </li>
-        <li>
-            <div class="title">給与形態</div>
-            <div class="list" v-for="item in items" :key="item.salary">
-                <div class="list-nav">{{item.salary}}</div>
-            </div>
-        </li>
+            <li>
+                <div class="title">
+                    求人ID
+                <button class="button" @click="changeOrder">⇅</button>
+                </div>
+                <div class="list" v-for="item in sortedItems" :key="item.id">
+                    <div class="list-nav">{{item.id}}</div>
+                </div>
+            </li>
+            <li>
+                <div class="title">クライアント名</div>
+                <div class="list" v-for="item in items" :key="item.name">
+                    <div class="list-nav">{{item.name}}</div>
+                </div>
+            </li>
+            <li>
+                <div class="title">ステータス</div>
+                <div class="list" v-for="item in items" :key="item.status">
+                    <div class="list-nav">{{item.status}}</div>
+                </div>
+            </li>
+            <li>
+                <div class="title">人選期日</div>
+                <div class="list" v-for="item in items" :key="item.date">
+                    <div class="list-nav">{{item.date}}</div>
+                </div>
+            </li>
+            <li>
+                <div class="title">開始日</div>
+                <div class="list" v-for="item in items" :key="item.satrtDate">
+                    <div class="list-nav">{{item.satrtDate}}</div>
+                </div>
+            </li>
+            <li>
+                <div class="title">求人タイトル</div>
+                <div class="list" v-for="item in items" :key="item.title">
+                    <div class="list-nav">{{item.title}}</div>
+                </div>
+            </li>
+            <li>
+                <div class="title">最寄駅</div>
+                <div class="list" v-for="item in items" :key="item.station">
+                    <div class="list-nav">{{item.station}}</div>
+                </div>
+            </li>
+            <li>
+                <div class="title">雇用期間</div>
+                <div class="list" v-for="item in items" :key="item.period">
+                    <div class="list-nav">{{item.period}}</div>
+                </div>
+            </li>
+            <li>
+                <div class="title">研修有無</div>
+                <div class="list" v-for="item in items" :key="item.training">
+                    <div class="list-nav">{{item.training}}</div>
+                </div>
+            </li>
+            <li>
+                <div class="title">給与形態</div>
+                <div class="list" v-for="item in items" :key="item.salary">
+                    <div class="list-nav">{{item.salary}}</div>
+                </div>
+            </li>
         </draggable>
     </div>
 </div>
@@ -72,87 +84,134 @@ import draggable from 'vuedraggable';
 
 export default {
     name: 'Table',
-
- components: {
-    'draggable': draggable,
-  },
-  data(){ 
-      return {
-    items:[
-        {
-        id:"0000000000", 
-        name:"株式会社ウィルグループ", 
-        status:"見込み", 
-        date:"2021/04/01",
-        satrtDate:"2021/04/01",
-        title:"求人タイトル求人タイトル",
-        station:"丸の内線中野坂上駅",
-        period:"有期雇用（長期）",
-        training:"あり",
-        salary:"月給"},
-        {
-        id:"0000000000", 
-        name:"株式会社ウィルグループ", 
-        status:"見込み", 
-        date:"2021/04/01",
-        satrtDate:"2021/04/01",
-        title:"求人タイトル求人タイトル",
-        station:"丸の内線中野坂上駅",
-        period:"有期雇用（長期）",
-        training:"あり",
-        salary:"月給"},
-        {
-        id:"0000000000", 
-        name:"株式会社ウィルグループ", 
-        status:"見込み", 
-        date:"2021/04/01",
-        satrtDate:"2021/04/01",
-        title:"求人タイトル求人タイトル",
-        station:"丸の内線中野坂上駅",
-        period:"有期雇用（長期）",
-        training:"あり",
-        salary:"月給"},
-        {
-        id:"0000000000", 
-        name:"株式会社ウィルグループ", 
-        status:"見込み", 
-        date:"2021/04/01",
-        satrtDate:"2021/04/01",
-        title:"求人タイトル求人タイトル",
-        station:"丸の内線中野坂上駅",
-        period:"有期雇用（長期）",
-        training:"あり",
-        salary:"月給"},
-    ]
+    components: {
+        'draggable': draggable,
+    },
+    data() { 
+        return{
+            items:[
+                {
+                id:"0000000000", 
+                name:"株式会社ウィルグループ",
+                status:"見込み",
+                date:"2021/04/01",
+                satrtDate:"2021/04/01",
+                title:"求人タイトル求人タイトル",
+                station:"丸の内線中野坂上駅",
+                period:"有期雇用（長期）",
+                training:"あり",
+                salary:"月給"
+                },
+                {
+                id:"0000000001", 
+                name:"株式会社ウィルグループ", 
+                status:"見込み", 
+                date:"2021/04/02",
+                satrtDate:"2021/04/01",
+                title:"求人タイトル求人タイトル",
+                station:"丸の内線中野坂上駅",
+                period:"有期雇用（長期）",
+                training:"あり",
+                salary:"月給"
+                },
+                {
+                id:"0000000002", 
+                name:"株式会社ウィルグループ", 
+                status:"見込み", 
+                date:"2021/04/03",
+                satrtDate:"2021/04/01",
+                title:"求人タイトル求人タイトル",
+                station:"丸の内線中野坂上駅",
+                period:"有期雇用（長期）",
+                training:"あり",
+                salary:"月給"
+                },
+                {
+                id:"0000000003", 
+                name:"株式会社ウィルグループ", 
+                status:"見込み", 
+                date:"2021/04/01",
+                satrtDate:"2021/04/01",
+                title:"求人タイトル求人タイトル",
+                station:"丸の内線中野坂上駅",
+                period:"有期雇用（長期）",
+                training:"あり",
+                salary:"月給"
+                },
+            ],
+            sortOrder: 1,
+            checked_items: [], 
+        }
+    },
+    computed: {
+        sortedItems(){
+            console.log(this.items);
+            return this.items.slice().sort((a, b) => {
+            return (a.id < b.id) ? -this.sortOrder : (a.id > b.id) ? this.sortOrder : 0;
+            })
+        },
+    },
+    methods: {
+        changeOrder(){
+            this.sortOrder = this.sortOrder > 0 ? -1 : 1;
+        },
     }
-  }
 }
 </script>
 
 <style>
-template {
-  padding : 15px;
+#box1 {
+    display: flex;
 }
+.check {
+    position: relative;
+}
+
+.checkbox-main {
+    position: absolute;
+    top: 10%;
+    left: 60%;
+}
+
+.checkbox {
+    margin: 20px;
+}
+
+.checkbox:first-child {
+    margin-top: 65px;
+}
+
+.title {
+    margin: 10px 0;
+    padding: 10px;
+    height: 10px;
+}
+
 ul {
     display: flex;
+    align-items: center;
     font-size: 8px;
 }
 
 li {
-  cursor:pointer;
-  padding: 10px;
-  list-style: none;
+    cursor: pointer;
+    list-style: none;
+
+}
+li .button {
+    margin: 0;
+    padding: 0;
+    border: none;
+    background-color: #ffffff;
 }
 
 li .list-nav {
   border-bottom: solid #ddd 1px;
   text-overflow: ellipsis;
   width: 10vw;
+  height: 10px;
   white-space: nowrap;
   overflow: hidden;
-}
-
-.del {
-  color: #f00;
+  padding: 20px 0;
 }
 </style>
