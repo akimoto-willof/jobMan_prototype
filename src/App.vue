326d12fa70fa382bd03305d6135f2fe24078662f
hiroyuki.akimoto <hiroyuki.akimoto@willof.co.jp>
2021-05-13 12:03:28 +0900

    カレンダーの実装

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,24 @@
   <div id="app">
     <Sideber />
     <Table />
+    <div class="calendar">
+      <Calendar />
+    </div>
   </div>
 </template>
 
 <script>
 import Table from '@/components/Table.vue';
 import Sideber from '@/components/Sideber.vue';
+import Calendar from '@/components/Calendar.vue';
 
 
 export default {
   name: 'App',
   components: {
     Table,
-    Sideber
+    Sideber,
+    Calendar
   }
 }
 </script>
@@ -25,5 +30,11 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   display: flex;
+}
+
+.calendar {
+  position: absolute;
+  top: 50%;
+  left: 50%;
 }
 </style>
---
+++ src/components/Calendar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+      <button @click="onAction">カレンダー</button>
+      <div v-show="active">
+            <datepicker
+                :language="ja"
+                :format="DatePickerFormat"></datepicker>
+      </div>
+  </div>
+
+</template>
+<script>
+import Datepicker from "vuejs-datepicker";
+import { ja } from 'vuejs-datepicker/dist/locale'
+
+export default {
+    name: 'calendar',
+    components: {
+        Datepicker
+    },
+    data() {
+        return {
+            active: true,
+            ja: ja,
+            DatePickerFormat: 'yyyy年MM月dd日'
+        }
+    },
+    methods: {
+        onAction(){
+            if(this.active) {
+                this.active = false
+            } else {
+                this.active = true
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
